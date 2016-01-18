27e94978979334d8a56f23dab5d0a687c3fd7e80
William Madruga <willmadruga@gmail.com>
2016-01-17 23:32:00 -0800

    Search tag is now being used to search both music and image.

--- script.js
+++ script.js
@@ -12,55 +12,56 @@ angular
       
       // _______________ DO NOT COMMIT YOUR SDK KEY !!!!!!!!!!!!!!!!!!!!!!!
       _500px.init({ 
-        sdk_key: 'DO NOT COMMIT YOUR SDK KEY !!!!!!!!!!!!!!!!!!!!!!!'
+        sdk_key: ''
       });
       // _______________ DO NOT COMMIT YOUR SDK KEY !!!!!!!!!!!!!!!!!!!!!!!
     });
 
-		$scope.searchSpotifyTag = function (searchTag) {
+    var searchSpotifyTag = function(searchTag) {
+      var spotifyWebserviceUrl = 'https://api.spotify.com/v1/search?q=' + searchTag + '&type=playlist';
+      $http.get(spotifyWebserviceUrl).then(
+        function(res) {
 
-			if (searchTag.length > 3) {
-
-        var spotifyWebserviceUrl = 'https://api.spotify.com/v1/search?q=' + searchTag + '&type=playlist';
-				$http.get(spotifyWebserviceUrl).then(
-					function(res) {
-            console.log(res);
-						var urls = [];
-						 res.data.playlists.items.forEach(function(e, i) {
-							 if (i <= 10) { // use first 10s only.
-								 urls.push('https://embed.spotify.com/?uri=' + e.uri);
-							 }
-						 });
-						$scope.trackUrls = urls;
-					},
-					function(error) {
-						console.log(error);
-					}
-				);
-			}
+          var urls = [];
+            res.data.playlists.items.forEach(function(e, i) {
+              if (i <= 10) { // use first 10s only.
+                urls.push('https://embed.spotify.com/?uri=' + e.uri);
+              }
+            });
+          $scope.trackUrls = urls;
+        },
+        function(error) {
+          console.log(error);
+        }
+      )
     };
     
-    $scope.getBackground = function() {
-      
-      _500px.api('/photos', { feature: 'popular', page: 1 }, function (response) {
+    $scope.getBackground = function(searchTag) {
+      _500px.api('/photos/search', { tag: searchTag, page: 1 }, function (response) {
         if (response.success) {
           
           var baseUrl = 'https://500px.com';
           var targetUrl = 'https://drscdn.500px.org/photo/';
-          var photo = response.data.photos[0];
-          var resultPageUrl = baseUrl + photo.url;
           
+          // get a random image out of the result-set.
+          var topResults = response.data.photos.length;
+          var photo = response.data.photos[getRandom(0, topResults)];
+          var resultPageUrl = baseUrl + photo.url;
+
+          // the photo.url they return is a shitty image...
+          // let's get hack and get a good one from the user page.          
           $http.get(resultPageUrl).then(
             function(res) {
-              var data = res.data;
-              console.log(data);
-              var startIndex = data.search(targetUrl);
-              var imgSrcStart = data.substr(startIndex);
+              var startIndex = res.data.search(targetUrl);
+              var imgSrcStart = res.data.substr(startIndex);
               var endIndex = imgSrcStart.search("'");
               var imgUrl = imgSrcStart.substr(0, endIndex);
               document.body.background = imgUrl;
             }
           );
+          
+          searchSpotifyTag(searchTag);
+          hideSearch();
           
         } else {
           console.log(response);
@@ -69,4 +70,20 @@ angular
       
     }
     
-  }]);
+  }]
+);
+  
+
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+var getRandom = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// hide the search elements    
+var hideSearch = function() {
+  
+  var searchInput = document.querySelectorAll('#searchBar');
+  searchInput[0].style.display = 'none';
+  
+};
